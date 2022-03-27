399d3cc0aad35a2216432987d9b7457be7582f8b
mvs22 <mvs2@cin.ufpe.br>
2022-03-27 00:33:10 -0300

    Adicionando novos sprites e ajustando redimensionamento do cenario

--- Assets/RubyController.cs
+++ Assets/RubyController.cs
@@ -32,8 +32,8 @@ public class RubyController : MonoBehaviour
 
         //Debug.Log(horizontal, vertical);
         Vector2 position = transform.position;
-        position.x = position.x + 0.8f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.8f * vertical * Time.deltaTime;
+        position.x = position.x + 1f * horizontal * Time.deltaTime;
+        position.y = position.y + 1f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
